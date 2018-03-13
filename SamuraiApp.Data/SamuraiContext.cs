3ae36d77b0632d54d476400849d9e5704bdb10cb
Shalini Gupta <HZNQLB@NAM.corp.gm.com>
2018-03-12 20:03:00 -0500

    adding files

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    internal class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+
+        protected override void OnConfiguring( DbContextOptionsBuilder optionsBuilder )
+        {
+            optionsBuilder.UseSqlCe(
+                 "Server = (localdb)\\mssqllocaldb; Database = SamuraiData; Trusted_Connection = True; " );
+        }
+    }
+}
